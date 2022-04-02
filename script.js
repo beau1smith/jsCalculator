c684fe4b7e0f367484d71f71f47b964131891b80
Beau Smith <beau1smith@gmail.com>
2022-04-02 19:10:21 -0400

    wrote functions that provide basic functionality for all of the calculator functions except decimal points. TODO: Refactor everything. Focus on DRY & SOLID principles.

--- script.js
+++ script.js
@@ -14,53 +14,93 @@ function divide(a, b){
   return a / b;
 }
 
-function addToDisplay(input){
-  let userInput = document.createTextNode(input);
-  let display = document.querySelector('display');
-  display.appendChild(userInput);
-}
-
-function operate(number1, operator, number2){
-  let n1 = number1;
-  let n2 = number2;
-  if (operator === '+'){
-    return add(n1, n2);
-  }
-  if (operator === '-'){
-    return subtract(n1, n2);
-  }
-  if (operator === '*'){
-    return multiply(n1, n2);
-  }
-  if (operator === '/'){
-    return divide(n1, n2);
-  }
-}
-
 let inputString1 = '';
 let inputString2 = '';
+let operatorChoice = '';
 
+const calculatorNumbers = Array.from(document.getElementsByClassName('number'));
+const calculatorOperators = Array.from(document.getElementsByClassName('operator'));
+const calculatorEqual = Array.from(document.getElementsByClassName('equal'));
+const calculatorClear = Array.from(document.getElementsByClassName('clear'));
+const display = document.querySelector('.display');
 
-const buttons = Array.from(document.getElementsByClassName('button'));
-
-buttons.map( button => {
+calculatorNumbers.map( button => {
   button.addEventListener('click', (e) => {
-    const display = document.querySelector('.display');
     let inputType = e.target.classList;
-
-    if (inputType.contains('clear')) {
-      inputString = '';
-    } else if (inputType.contains('number') && inputString.length < 9){
-      let inputValue = e.target.innerText.toString();
-      inputString += inputValue;
+    let inputValue = e.target.innerText.toString();
+    if (inputString1.length < 9 && operatorChoice === '') {
+      inputString1 += inputValue;
+      display.innerText = inputString1;
+    } else {
+      if (inputString2.length < 9 && operatorChoice != ''){
+        inputString2 += inputValue;
+        display.innerText = inputString2;
+      }
     }
-
-
-
-
-
-        display.innerText = inputString;
-
-
+    console.log(`inputString1 is ${inputString1}`);
+    console.log(`inputString2 is ${inputString2}`);
+    console.log(`operatorChoice is ${operatorChoice}`);
     });
 });
+
+calculatorOperators.map( button => {
+  button.addEventListener('click', (e) => {
+    let inputType = e.target.classList;
+    let inputValue = e.target.innerText.toString();
+      if(inputValue == '%'){
+        inputString1 *= 0.01;
+        display.innerText = inputString1;
+      } else if (inputValue == '+ / -'){
+        inputString1 = -inputString1;
+        display.innerText = inputString1;
+        console.log(inputString1);
+      } else if (inputValue == '.'){
+          inputString1 += '.';
+      }
+      else {
+        display.innerText = '';
+        operatorChoice =  inputValue;
+      }
+    });
+  });
+
+
+calculatorClear.map( button => {
+  button.addEventListener('click', (e) => {
+    let inputType = e.target.classList;
+    let inputValue = e.target.innerText.toString();
+      inputString1 = '';
+      inputString2 = '';
+      operatorChoice = '';
+      display.innerText = '';
+      console.log(`inputString1 is ${inputString1}`);
+      console.log(`inputString2 is ${inputString2}`);
+      console.log(`operatorChoice is ${operatorChoice}`);
+  });
+});
+
+calculatorEqual.map( button => {
+  button.addEventListener('click', (e) => {
+    let inputType = e.target.classList;
+    let inputValue = e.target.innerText.toString();
+
+    console.log("equal button pressed");
+    if (inputString1 && inputString2 && operatorChoice){
+      if(operatorChoice === "+"){
+        result = add(parseInt(inputString1), parseInt(inputString2));
+      } else if (operatorChoice === "-"){
+        result = subtract(inputString1, inputString2);
+      } else if (operatorChoice === "*") {
+        result = multiply(inputString1, inputString2);
+      } else if (operatorChoice === "/") {
+      result = divide(inputString1, inputString2);
+      }
+    }
+
+    display.innerText = result;
+    inputString1 = result;
+    inputString2 = '';
+    operatorChoice = '';
+
+    });
+  });
